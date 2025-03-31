cd6f0569599d9a1d2932d1e25437fccd9c2b3446
ShudhanshuSingh <shudhanshusingh051@gmail.com>
2025-03-31 15:44:47 +0530

    Test

--- public/stockfishWorker.js
+++ public/stockfishWorker.js
@@ -1,9 +1,20 @@
 importScripts('stockfish-nnue-16.js');
 
-self.onmessage = function (e) {
-  stockfish.postMessage(e.data);
+// Detect if Stockfish is a function (i.e., Stockfish() returns the engine)
+// OR just a preloaded Worker object
+const engine =
+  typeof Stockfish === 'function'
+    ? Stockfish() // multithreaded version
+    : (typeof stockfish !== 'undefined' ? stockfish : null);
+
+if (!engine) {
+  throw new Error("Stockfish engine could not be initialized.");
+}
+
+onmessage = function (e) {
+  engine.postMessage(e.data);
 };
 
-stockfish.onmessage = function (e) {
-  self.postMessage(e.data);
+engine.onmessage = function (e) {
+  postMessage(e.data);
 };
